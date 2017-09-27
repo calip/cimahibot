c7f1cde0a796c46c38c9f4d8259eb1f29f4daee7
alip putra <alip.putra@tes.co.id>
2017-09-27 19:42:28 +0700

    fix help command

--- index.js
+++ index.js
@@ -12,22 +12,28 @@ bot.onText(/\/wisata/, function(msg, match) {
 });
 
 bot.onText(/\/punten/, function(msg, match) {
-  	var fromId = msg.from.id;
-	var chatId = msg.chat.id;
-  	bot.sendMessage(fromId, "Wilujeng Sumping " + msg.from.first_name);
-  	bot.sendMessage(chatId, "Wilujeng Sumping " + msg.from.first_name);
+    var fromId = msg.from.id;
+    var chatId = msg.chat.id;
+    bot.sendMessage(fromId, "Wilujeng Sumping " + msg.from.first_name);
+    bot.sendMessage(chatId, "Wilujeng Sumping " + msg.from.first_name);
 });
 
-bot.onText(/\/aya/, (msg) => {
-	bot.sendPhoto(msg.chat.id,"https://upload.wikimedia.org/wikipedia/commons/f/f6/Logo-Cimahi.png" );
+bot.onText(/\/help/, function(msg, match) {
+    var fromId = msg.from.id;
+    var chatId = msg.chat.id;
+    bot.sendMessage(fromId, "You can control me by sending these commands: \n /punten - kenalan \n /wisata - cari tempat wisata di cimahi \n /cimahi - sejarah cimahi");
+    bot.sendMessage(chatId, "You can control me by sending these commands: \n /punten - kenalan \n /wisata - cari tempat wisata di cimahi \n /cimahi - sejarah cimahi");
+});
+
+bot.onText(/\/cimahi/, (msg) => {
+  bot.sendPhoto(msg.chat.id,"https://upload.wikimedia.org/wikipedia/commons/f/f6/Logo-Cimahi.png" );
     
 });
 
 bot.on('message', (msg) => {
-	var questions = "naon";
-	if (msg.text.toLowerCase().includes(questions)) {
-		bot.sendMessage(msg.chat.id, "Hoyong naon " + msg.from.first_name + '?'); 
-	} 
+  var questions = "naon";
+  if (msg.text.toLowerCase().includes(questions)) {
+    bot.sendMessage(msg.chat.id, "Hoyong naon " + msg.from.first_name + '?'); 
+  } 
 
 });
-
