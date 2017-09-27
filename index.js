92e83bee9172d7c0714ce148c2ffdce2dfe51a23
alip putra <alip.putra@tes.co.id>
2017-09-27 19:27:43 +0700

    simple responsive bot

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-const token = '459952409:AAGD4N1hVn5i_epkc_ieDPqDuswXl6vx0sE';
-// Setup polling way
-const bot = new TelegramBot(token, {port: true});
-
-bot.onText(/\/tes/, function(msg, match) {
+const TelegramBot = require('node-telegram-bot-api'); 
+const token = 'YOUR_BOT_TOKEN';
+const bot = new TelegramBot(token, {polling: true});
+    
+bot.onText(/\/wisata/, function(msg, match) {
   var fromId = msg.from.id;
-  var insults = ["Dumbass", "you were the fastest?", "Look, you aint funny. Your life is just a joke."];
+  var chatId = msg.chat.id;
+  var insults = ["Stasiun Cimahi", "Alam Wisata Cimahi", "Bioskop Rio"];
   var chosenInsult = insults[Math.floor(Math.random() * insults.length)];
   bot.sendMessage(fromId, chosenInsult);
+  bot.sendMessage(chatId, chosenInsult);
 });
 
-bot.onText(/\/help/, function(msg, match) {
-  var fromId = msg.from.id;
-  bot.sendMessage(fromId, "This spectacular bot just have one single command.\n/tes - Insult you.");
+bot.onText(/\/punten/, function(msg, match) {
+  	var fromId = msg.from.id;
+	var chatId = msg.chat.id;
+  	bot.sendMessage(fromId, "Wilujeng Sumping " + msg.from.first_name);
+  	bot.sendMessage(chatId, "Wilujeng Sumping " + msg.from.first_name);
 });
+
+bot.onText(/\/aya/, (msg) => {
+	bot.sendPhoto(msg.chat.id,"https://upload.wikimedia.org/wikipedia/commons/f/f6/Logo-Cimahi.png" );
+    
+});
+
+bot.on('message', (msg) => {
+	var questions = "naon";
+	if (msg.text.toLowerCase().includes(questions)) {
+		bot.sendMessage(msg.chat.id, "Hoyong naon " + msg.from.first_name + '?'); 
+	} 
+
+});
+
